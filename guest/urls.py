a7e8f77f4c9c7da76576ff6c21c91339ba0f6555
chideegit <chidiebereohiri@gmail.com>
2021-07-28 14:44:04 +0100

    More codes added

--- guest/urls.py
+++ guest/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import AddGuestView, AllGuestView, GuestHistoryView, SignOutGuestView
+
+urlpatterns = [
+    path('', AllGuestView, name='all-guests'),
+    path('add-guest/', AddGuestView, name='add-guest'),
+    path('guest-history/', GuestHistoryView, name='guest-history'),
+    path('signout-guest/<int:pk>/', SignOutGuestView, name='signout-guest')
+]
 
